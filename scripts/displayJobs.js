69f1dac1d6452b100b6f1d26c62429c858b52a05
Ammar-Abode <ammar-abode@ammar-abode-linux.scoiumtech.local>
2024-07-30 18:58:43 +0300

    intial commit

--- scripts/displayJobs.js
+++ scripts/displayJobs.js
@@ -2,6 +2,14 @@ export function displayJobs(jobs) {
   const jobListings = document.getElementById("job-listings");
   jobListings.innerHTML = "";
 
+  if (jobs.length === 0) {
+    const noItemsMessage = document.createElement("dic");
+    noItemsMessage.classList.add("no-items");
+    noItemsMessage.textContent = "No items found";
+    jobListings.appendChild(noItemsMessage);
+    return;
+  }
+
   jobs.forEach((job) => {
     const jobCard = document.createElement("div");
     jobCard.classList.add("job-card");
@@ -10,37 +18,36 @@ export function displayJobs(jobs) {
     }
 
     jobCard.innerHTML = `
-              <div class="job-header">
-                  <img src="${job.logo}" alt="${job.company} logo">
-                  <div class="job-info">
-                      <div class="job-header">
-                          <span class="job-company">${job.company}</span>
-                          ${job.new ? '<span class="job-new">NEW!</span>' : ""}
-                          ${
-                            job.featured
-                              ? '<span class="job-featured">FEATURED</span>'
-                              : ""
-                          }
-                      </div>
-                      <h2 class="job-position">${job.position}</h2>
-                      <div class="job-details">
-                          <span class="job-posted-time">${job.postedAt}</span>
-                          <span class="job-contract">${job.contract}</span>
-                          <span class="job-location">${job.location}</span>
-                      </div>
-                  </div>
-              </div>
-              <div class="job-tags">
-                  <span class="job-role">${job.role}</span>
-                  <span class="job-level">${job.level}</span>
-                  ${job.languages
-                    .map((lang) => `<span class="job-language">${lang}</span>`)
-                    .join("")}
-                  ${job.tools
-                    .map((tool) => `<span class="job-tool">${tool}</span>`)
-                    .join("")}
-              </div>
-          `;
+        <div class="job-header">
+          <img src="${job.logo}" alt="${job.company} logo">
+          <div class="job-info">
+            <div class="job-header">
+              <span class="job-company">${job.company}</span>
+              ${job.new ? '<span class="job-new">NEW!</span>' : ""}
+              ${
+                job.featured ? '<span class="job-featured">FEATURED</span>' : ""
+              }
+            </div>
+            <h2 class="job-position">${job.position}</h2>
+            <div class="job-details">
+              <span class="job-posted-time">${job.postedAt}</span>
+              <span class="job-contract">${job.contract}</span>
+              <span class="job-location">${job.location}</span>
+            </div>
+          </div>
+        </div>
+        <div class="job-tags">
+          <span class="job-role">${job.role}</span>
+          <span class="job-level">${job.level}</span>
+          ${job.languages
+            .map((lang) => `<span class="job-language">${lang}</span>`)
+            .join("")}
+          ${job.tools
+            .map((tool) => `<span class="job-tool">${tool}</span>`)
+            .join("")}
+        </div>
+      `;
+
     jobListings.appendChild(jobCard);
   });
 }
