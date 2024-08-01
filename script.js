dd5aa4bb8dc9acff4e972cc8cab6ee6b060655b9
Ahmad Jallad <ahmadjallad@ahmad-jallad-linux.scoiumtech.local>
2024-08-01 12:37:39 +0300

    modify script.js and styles.css

--- script.js
+++ script.js
@@ -1,177 +1,116 @@
-
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-      const response = await fetch('data.json');
-      const jobListings = await response.json();
-      window.jobListings = jobListings;
-      displayJobListings(jobListings);
-    } catch (error) {
-      console.error('Error fetching job listings:', error);
+  try {
+    const response = await fetch('data.json');
+    const jobListings = await response.json();
+    window.jobListings = jobListings;
+    displayJobListings(jobListings);
+  } catch (error) {
+    console.error('Error fetching job listings:', error);
+  }
+});
+
+let selectedFilters = [];
+
+function displayJobListings(jobListings) {
+  const container = document.getElementById('job-listings-container');
+  container.innerHTML = '';
+  jobListings.forEach(job => {
+    const jobListing = createJobListing(job);
+    container.appendChild(jobListing);
+  });
+}
+
+function displayJobFilter(element) {
+  const filter = document.getElementById('job-filter');
+  const filterTags = filter.querySelector('.filter-tags');
+
+  filter.style.padding = "20px";
+  filter.style.width = "75%";
+
+  const elementClone = element.cloneNode(true);
+  const deleteBtn = document.createElement('button');
+  deleteBtn.className = 'delete-button';
+  deleteBtn.innerHTML = 'X';
+  
+
+  filterTags.appendChild(elementClone);
+  filterTags.appendChild(deleteBtn);
+
+  selectedFilters.push(element.textContent);
+  filterJobListings();
+
+  deleteBtn.addEventListener('click', () => {
+    filterTags.removeChild(elementClone);
+    selectedFilters = selectedFilters.filter(filter => filter !== element.textContent);
+    deleteBtn.remove();
+    filterJobListings();
+  });
+
+  if (!filter.querySelector('.clear')) {
+    const clearBtn = document.createElement('button');
+    clearBtn.innerHTML = "Clear";
+    clearBtn.className = 'clear';
+    clearBtn.addEventListener('click', () => {
+      filterTags.innerHTML = '';
+      filter.style.padding = "0";
+      filter.style.width = "0";
+      clearBtn.remove();
+      selectedFilters = [];
+      filterJobListings();
+    });
+    filter.appendChild(clearBtn);
+  }
+}
+
+
+
+function filterJobListings() {
+  if (selectedFilters.length === 0) {
+    displayJobListings(window.jobListings);
+    return;
+  }
+
+  const filteredListings = window.jobListings.filter(job => {
+    const jobTags = [job.role, job.level, ...job.languages, ...job.tools];
+    return selectedFilters.every(filter => jobTags.includes(filter));
+  });
+
+  displayJobListings(filteredListings);
+}
+
+
+
+function createJobListing(job) {
+  const jobDiv = document.createElement('div');
+  jobDiv.className = 'job-listing';
+  jobDiv.innerHTML = `
+    <img src="${job.logo}" alt="${job.company} logo">
+
+    <div class="job-details">
+      <div class="company-name">
+        <h2>${job.company}</h2>
+        ${job.new ? '<span class="new">NEW!</span>' : ''}
+        ${job.featured ? '<span class="featured">FEATURED</span>' : ''}
+      </div>
+      <h3>${job.position}</h3>
+      <p>${job.postedAt} • ${job.contract} • ${job.location}</p>
+    </div>
+
+    <div class="tags">
+        <span class="tag">${job.role}</span>
+        <span class="tag">${job.level}</span>
+        ${job.languages.map(language => `<span class="tag">${language}</span>`).join('')}
+        ${job.tools.map(tool => `<span class="tag">${tool}</span>`).join('')}
+    </div>
+  `;
+
+  const tagsContainer = jobDiv.querySelector('.tags');
+  tagsContainer.addEventListener('click', event => {
+    if (event.target.classList.contains('tag')) {
+      displayJobFilter(event.target);
     }
   });
   
 
-
-  let selectedFilters = [];
-
-
-  
-  function displayJobListings(jobListings) {
-    const container = document.getElementById('job-listings-container');
-    container.innerHTML = ''; 
-    jobListings.forEach(job => {
-      const jobListing = createJobListing(job);
-      container.appendChild(jobListing);
-    });
-  }
-
-
-  
-  function displayJobFilter(element) {
-    const filter = document.getElementById('job-filter');
-    filter.style.padding = "20px";
-    filter.style.width = "75%";
-  
-    const filterTags = filter.querySelector('.filter-tags');
-    const elementClone = element.cloneNode(true);
-    filterTags.appendChild(elementClone);
-  
-    const deleteBtn = document.createElement('button');
-    deleteBtn.className = 'delete-button';
-    deleteBtn.innerHTML = 'X';
-    filterTags.appendChild(deleteBtn);
-  
-    selectedFilters.push(element.textContent);
-    filterJobListings();
-  
-    deleteBtn.addEventListener('click', () => {
-      filterTags.removeChild(elementClone);
-      filterTags.removeChild(deleteBtn);
-      selectedFilters = selectedFilters.filter(filter => filter !== element.textContent);
-      filterJobListings();
-    });
-  
-    if (!document.querySelector('.clear')) {
-      const clear = document.createElement('button');
-      clear.innerHTML = "clear";
-      clear.className = 'clear';
-      clear.addEventListener('click', () => {
-        filterTags.innerHTML = '';
-        filter.style.padding = "0";
-        filter.style.width = "0";
-        // filter.innerHTML='';
-        clear.remove();
-        selectedFilters = [];
-        filterJobListings();
-      });
-      filter.appendChild(clear);
-    }
-  }
-
-  
-  function filterJobListings() {
-    if (selectedFilters.length === 0) {
-      displayJobListings(window.jobListings);
-      return;
-    }
-  
-    const filteredListings = window.jobListings.filter(job => {
-      const jobTags = [job.role, job.level, ...job.languages, ...job.tools];
-      return selectedFilters.every(filter => jobTags.includes(filter));
-    });
-
-    displayJobListings(filteredListings);
-  }
-
-  
-  
-  function createJobListing(job) {
-    const jobDiv = document.createElement('div');
-    jobDiv.className = 'job-listing';
-  
-    const companyLogo = document.createElement('img');
-    companyLogo.src = job.logo;
-    companyLogo.alt = `${job.company} logo`;
-    jobDiv.appendChild(companyLogo);
-  
-    const jobDetails = document.createElement('div');
-    jobDetails.className = 'job-details';
-    
-    const companyName = document.createElement('div');
-    companyName.className = 'company-name';
-    const companyNameH2 = document.createElement('h2');
-    companyNameH2.textContent = job.company;
-    companyName.appendChild(companyNameH2);
-  
-    if (job.new) {
-      const newSpan = document.createElement('span');
-      newSpan.className = 'new';
-      newSpan.textContent = 'NEW!';
-      companyName.appendChild(newSpan);
-    }
-    if (job.featured) {
-      const featuredSpan = document.createElement('span');
-      featuredSpan.className = 'featured';
-      featuredSpan.textContent = 'FEATURED';
-      companyName.appendChild(featuredSpan);
-    }
-    jobDetails.appendChild(companyName);
-  
-    const position = document.createElement('h3');
-    position.textContent = job.position;
-    jobDetails.appendChild(position);
-  
-    const details = document.createElement('p');
-    details.innerHTML = `
-      ${job.postedAt} • ${job.contract} • ${job.location}
-    `;
-    jobDetails.appendChild(details);
-  
-    const tags = document.createElement('div');
-    tags.className = 'tags';
-  
-    const languages = document.createElement('div');
-    languages.className = 'languages';
-    job.languages.forEach(language => {
-      const languageTag = document.createElement('span');
-      languageTag.className = 'tag';
-      languageTag.textContent = language;
-      languageTag.addEventListener('click', () => displayJobFilter(languageTag));
-      languages.appendChild(languageTag);
-    });
-  
-    const roles = document.createElement('div');
-    roles.className = 'roles';
-    const level = document.createElement('span');
-    level.textContent = job.level;
-    level.className = 'tag';
-    level.addEventListener('click', () => displayJobFilter(level));
-  
-    const role = document.createElement('span');
-    role.textContent = job.role;
-    role.className = 'tag';
-    role.addEventListener('click', () => displayJobFilter(role));
-    roles.appendChild(role);
-    roles.appendChild(level);
-    tags.appendChild(roles);
-    tags.appendChild(languages);
-  
-    const tools = document.createElement('div');
-    tools.className = 'tools';
-    job.tools.forEach(tool => {
-      const toolTag = document.createElement('span');
-      toolTag.className = 'tag';
-      toolTag.textContent = tool;
-      toolTag.addEventListener('click', () => displayJobFilter(toolTag));
-      tools.appendChild(toolTag);
-    });
-    tags.appendChild(tools);
-  
-    jobDiv.appendChild(jobDetails);
-    jobDiv.appendChild(tags);
-  
-    return jobDiv;
-  }
-  
-
-
+  return jobDiv;
+}
